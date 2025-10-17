c09c27c84b305b025f0d4d99576b6e86dda20ae4
nguyennam <nguyensonnam1205@gmail.com>
2025-10-17 11:14:18 +0700

    add
    add

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public UI_SkillToolTip skillToolTip { get; private set; }
+    public UI_ItemToolTip itemToolTip { get; private set; }
+    public UI_StatToolTip statToolTip { get; private set; }
+
+    public UI_SkillTree skillTreeUI { get; private set; }
+    public UI_Inventory inventoryUI { get; private set; }
+
+    private bool skillTreeEnabled;
+    private bool inventoryEnabled;
+
+    private void Awake()
+    {
+        itemToolTip = GetComponentInChildren<UI_ItemToolTip>();
+        skillToolTip = GetComponentInChildren<UI_SkillToolTip>();
+        statToolTip = GetComponentInChildren<UI_StatToolTip>();
+
+        skillTreeUI = GetComponentInChildren<UI_SkillTree>(true);
+        inventoryUI = GetComponentInChildren<UI_Inventory>(true);
+
+        skillTreeEnabled = skillTreeUI.gameObject.activeSelf;
+        inventoryEnabled = inventoryUI.gameObject.activeSelf;
+    }
+
+    public void ToggleSkillTreeUI()
+    {
+        skillTreeEnabled = !skillTreeEnabled;
+        skillTreeUI.gameObject.SetActive(skillTreeEnabled);
+        skillToolTip.ShowToolTip(false, null);
+    }
+
+    public void ToggleInventoryUI()
+    {
+        inventoryEnabled = !inventoryEnabled;
+        inventoryUI.gameObject.SetActive(inventoryEnabled);
+        statToolTip.ShowToolTip(false, null);
+        itemToolTip.ShowToolTip(false, null);
+    }
+}
